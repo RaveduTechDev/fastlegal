08d955948d7036bde44471b9f44b8bf0c1fdce8f
Ariel Gema Wardana <2024.arielgemaw.rplb@student.unsia.ac.id>
2024-07-08 23:36:45 +0700

    extract css js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="{{ asset('lib/owlcarousel/css/owl.carousel.min.css') }}">
     <link rel="stylesheet" href="{{ asset('lib/owlcarousel/css/owl.theme.default.min.css') }}">
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-    <link rel="stylesheet" href="{{ asset('build/assets/app-Dnxray3M.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-B3-_X3JM.css') }}">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/css/lightgallery.min.css" />
 
@@ -30,8 +30,7 @@
     <script src="{{ asset('lib/jquery.min.js') }}"></script>
     <script src="{{ asset('lib/owlcarousel/js/owl.carousel.min.js') }}"></script>
     <script src="{{ url('https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/js/lightgallery.min.js') }}"></script>
-
-    <script src="{{ asset('build/assets/app-CwKecaEP.js') }}"></script>
+    <script src="{{ asset('build/assets/app-1COdtq9U.js') }}"></script>
 
 </body>
 
