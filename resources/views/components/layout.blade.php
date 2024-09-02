f02bdcc0d02a97cb8f352e45dea7ed3ad41f0397
Ariel Gema Wardana <2024.arielgemaw.rplb@student.unsia.ac.id>
2024-09-02 13:50:31 +0700

    add: comment

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    {{-- SEO to share link --}}
     @if (request()->is('artikel/detail/*'))
         <meta property="og:title" content="{{ $article->title }}" />
         <meta property="og:url" content="https://fastlegal.id/artikel/detail/{{ $article->slug }}" />
