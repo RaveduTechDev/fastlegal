821b7d9649173b80b06df86e75f9a2d97cb41155
Ariel Gema Wardana <2024.arielgemaw.rplb@student.unsia.ac.id>
2026-03-11 23:13:55 +0700

    refactor: fix icon wa

--- resources/views/components/wa.blade.php
+++ resources/views/components/wa.blade.php
@@ -1,5 +1,5 @@
 <div id="whatsapp-button" class="fixed bottom-5 right-5 md:right-8 z-30">
-    <a href="{{ url('https://api.whatsapp.com/send?phone=6282225288269&text=Halo,%20saya%20ingin%20bertanya%20mengenai%20layanan%20FASTLEGAL') }}"
+    <a href="{{ url('https://api.whatsapp.com/send?phone=6281294924995&text=Halo,%20saya%20ingin%20bertanya%20mengenai%20layanan%20FASTLEGAL') }}"
         target="_blank">
         <div
             class="relative inline-flex items-center justify-center p-3 rounded-full shadow-lg bg-green-500 hover:bg-green-600">
@@ -11,7 +11,7 @@
                 <path fill="currentColor"
                     d="M16.735 13.492c-.038-.018-1.497-.736-1.756-.83a1.008 1.008 0 0 0-.34-.075c-.196 0-.362.098-.49.291-.146.217-.587.732-.723.886-.018.02-.042.045-.057.045-.013 0-.239-.093-.307-.123-1.564-.68-2.751-2.313-2.914-2.589-.023-.04-.024-.057-.024-.057.005-.021.058-.074.085-.101.08-.079.166-.182.249-.283l.117-.14c.121-.14.175-.25.237-.375l.033-.066a.68.68 0 0 0-.02-.64c-.034-.069-.65-1.555-.715-1.711-.158-.377-.366-.552-.655-.552-.027 0 0 0-.112.005-.137.005-.883.104-1.213.311-.35.22-.94.924-.94 2.16 0 1.112.705 2.162 1.008 2.561l.041.06c1.161 1.695 2.608 2.951 4.074 3.537 1.412.564 2.081.63 2.461.63.16 0 .288-.013.4-.024l.072-.007c.488-.043 1.56-.599 1.804-1.276.192-.534.243-1.117.115-1.329-.088-.144-.239-.216-.43-.308Z" />
             </svg>
-
+            <span class="sr-only">Chat on WhatsApp</span>
         </div>
     </a>
 </div>
