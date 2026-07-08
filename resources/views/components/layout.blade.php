ef501bd368f9ba910fa710f9a90a57359a0a47a0
Ariel Gema Wardana <2024.arielgemaw.rplb@student.unsia.ac.id>
2026-07-08 12:35:21 +0700

    fix: change link css and js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -39,7 +39,9 @@
 
     {{-- <link rel="stylesheet" href="{{ asset('lib/owlcarousel/css/owl.theme.default.min.css') }}"> --}}
     @livewireScriptConfig
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    <link rel="stylesheet" href="{{ asset('build/assets/app-C6yo_0wT.css') }}">
+    <script src="{{ asset('build/assets/app-BGFk6IQn.js') }}"></script>
 
 </head>
 
